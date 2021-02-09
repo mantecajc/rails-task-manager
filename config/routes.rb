88b43901301ef7b5b3421704d7766d464e473122
Javier Cuadrado <mantecajc@gmail.com>
2021-02-09 14:06:45 +0100

    Refactored code

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  # Edit
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  # Delte
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # get 'tasks', to: 'tasks#index'
+
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+  # # The show route need to be under the new
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # # Edit
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
